5e0a96cc06f926d4d57a155a4f32c4ef5e6da1b1
孙大圣 <sclcoder@163.com>
2016-08-25 17:38:51 +0800

    Initial Commit

---
+++ MMMMM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMMMM
+//
+//  Created by jmit on 16/8/25.
+//  Copyright © 2016年 godHands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
